b108df18b238e521cfa1a30fc887950b7c44d19c
dazzy-ji <d.indimuli@alustudent.com>
2026-06-11 05:30:03 -0700

    SIGINT created

--- setup_project.sh
+++ setup_project.sh
@@ -11,5 +11,18 @@ cleanup_on_interupt() {
 
 	if [ -d "$PROJECT_DIR" ]; then
 		echo "Current state of '$PROJECT_DIR' is being archived -> ${ARCHIVE_NAME}.tar.gz"
-
+		if tar -czf "${ARCHIVE_NAME}.tar.gz" "$PROJECT_DIR" 2>/dev/null; then
+			echo "Archive created"
+		else
+			echo "Failed to create archive"
+		fi
+		
+		echo "Incomplete directory '$PROJECT_DIR' is being deleted"
+		rm -rf "$PROJECT_DIR"
+		echo "Workspace cleaned"
+	else
+		echo"There is no current directory to be cleaned up."
+	fi
+	exit 130
 }
+trap cleanup_on_interrupt SIGINT
